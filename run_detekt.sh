cbfe3f3562c773b4bfdd0783494bcd47db5c8a34
dustin <dustinshimono@gmail.com>
2020-11-01 14:53:00 -0500

    chore: remove broken command in run_detekt.sh

--- run_detekt.sh
+++ run_detekt.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# add default ruleset if not specified
-pc_args="${*:1:idx-1}"
-if [[ ! $pc_args == *"-R "* ]]; then
-  pc_args="$pc_args -R /opt/detekt/ruleset.xml"
-fi
+# TODO add support for optional arguments
 
 /opt/detekt --auto-correct true --config /opt/detekt/detekt-config.yml --excludes "**/generated/**" --jvm-target JVM_1_8 --plugins /opt/detekt/detekt-formatting.jar --version
