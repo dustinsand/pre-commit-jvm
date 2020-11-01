be64f44c7cdd02e65837f1a1d6effd24f722c062
dustin <dustinshimono@gmail.com>
2020-11-01 15:02:45 -0500

    chore: fix detekt CLI args

--- run_detekt.sh
+++ run_detekt.sh
@@ -2,4 +2,8 @@
 
 # TODO add support for optional arguments
 
-/opt/detekt/detekt --auto-correct true --config /opt/detekt/detekt-config.yml --excludes "**/generated/**" --jvm-target JVM_1_8 --plugins /opt/detekt/detekt-formatting.jar --version
+/opt/detekt/detekt \
+  --auto-correct \
+  --config /opt/detekt/detekt-config.yml \
+  --excludes "**/generated/**" \
+  --plugins /opt/detekt/detekt-formatting.jar
