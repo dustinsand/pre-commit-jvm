cd7514932ee0c486987adcd0e6e62e714bfa3976
dustin <dustinshimono@gmail.com>
2020-11-01 14:54:12 -0500

    chore: fix path in run_detekt.sh

--- run_detekt.sh
+++ run_detekt.sh
@@ -2,4 +2,4 @@
 
 # TODO add support for optional arguments
 
-/opt/detekt --auto-correct true --config /opt/detekt/detekt-config.yml --excludes "**/generated/**" --jvm-target JVM_1_8 --plugins /opt/detekt/detekt-formatting.jar --version
+/opt/detekt/detekt --auto-correct true --config /opt/detekt/detekt-config.yml --excludes "**/generated/**" --jvm-target JVM_1_8 --plugins /opt/detekt/detekt-formatting.jar --version
