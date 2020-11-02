5d0471534acdd27e514055cdee9869360ee85aaf
dustin <dustinshimono@gmail.com>
2020-11-01 21:07:25 -0500

    fix: adding default args not working

--- run_detekt.sh
+++ run_detekt.sh
@@ -9,18 +9,12 @@ for (( i=1; i <= "$#"; i++ )); do
     fi
 done
 
-# add default config arguments if not specified
+# add minimum default config arguments if not specified
 detekt_args="${*:1:idx-1}"
-if [[ detekt_args != *"--auto-correct "* ]] && [[ detekt_args != *"-ac "* ]]; then
-  detekt_args="$detekt_args --auto-correct"
-fi
-if [[ detekt_args != *"--config "* ]] && [[ detekt_args != *"-c "* ]]; then
+if [[ ! detekt_args == *"--config "* ]] && [[ ! detekt_args == *"-c "* ]]; then
   detekt_args="$detekt_args --config /opt/detekt/detekt-config.yml"
 fi
-if [[ detekt_args != *"--plugins "* ]] && [[ detekt_args != *"-p "* ]]; then
-  detekt_args="$detekt_args --plugins /opt/detekt/detekt-formatting.jar"
-fi
 
-echo "Detect arguments: $detekt_args"
+echo "Passed detekt arguments: $detekt_args"
 
 /opt/detekt/detekt $detekt_args
